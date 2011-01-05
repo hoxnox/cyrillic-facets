b1bef6847e15f0e66fcce9cb9ceae4c927c916be
hoxnox <hoxnox@gmail.com>
2011-01-05 13:45:18 +0300

    bugfixing/ New test released. Tested.

--- codecvt/codecvt_cp1251.hpp
+++ codecvt/codecvt_cp1251.hpp
@@ -21,6 +21,72 @@ class codecvt_cp1251 : public std::codecvt<wchar_t, char, mbstate_t>
         noconv(std::codecvt_base::noconv)
     {
       // UNICODE -> CP1251 tab, extended part without alpha
+<<<<<<< HEAD
+      out_tab[0x0402] = 0x80;     in_tab[0x80] = 0x0402;
+      out_tab[0x0403] = 0x81;     in_tab[0x81] = 0x0403;
+      out_tab[0x201A] = 0x82;     in_tab[0x82] = 0x201A;
+      out_tab[0x0453] = 0x83;     in_tab[0x83] = 0x0453;
+      out_tab[0x201E] = 0x84;     in_tab[0x84] = 0x201E;
+      out_tab[0x2026] = 0x85;     in_tab[0x85] = 0x2026;
+      out_tab[0x2020] = 0x86;     in_tab[0x86] = 0x2020;
+      out_tab[0x2021] = 0x87;     in_tab[0x87] = 0x2021;
+      out_tab[0x20AC] = 0x88;     in_tab[0x88] = 0x20AC;
+      out_tab[0x2030] = 0x89;     in_tab[0x89] = 0x2030;
+      out_tab[0x0409] = 0x8A;     in_tab[0x8A] = 0x0409;
+      out_tab[0x2039] = 0x8B;     in_tab[0x8B] = 0x2039;
+      out_tab[0x040A] = 0x8C;     in_tab[0x8C] = 0x040A;
+      out_tab[0x040C] = 0x8D;     in_tab[0x8D] = 0x040C;
+      out_tab[0x040B] = 0x8E;     in_tab[0x8E] = 0x040B;
+      out_tab[0x040F] = 0x8F;     in_tab[0x8F] = 0x040F;
+      out_tab[0x0452] = 0x90;     in_tab[0x90] = 0x0452;
+      out_tab[0x2018] = 0x91;     in_tab[0x91] = 0x2018;
+      out_tab[0x2019] = 0x92;     in_tab[0x92] = 0x2019;
+      out_tab[0x201C] = 0x93;     in_tab[0x93] = 0x201C;
+      out_tab[0x201D] = 0x94;     in_tab[0x94] = 0x201D;
+      out_tab[0x2022] = 0x95;     in_tab[0x95] = 0x2022;
+      out_tab[0x2013] = 0x96;     in_tab[0x96] = 0x2013;
+      out_tab[0x2014] = 0x97;     in_tab[0x97] = 0x2014;
+      //out_tab[    ] = 0x98;   //in_tab[0x98] =     ;  
+      out_tab[0x2122] = 0x99;     in_tab[0x99] = 0x2122;
+      out_tab[0x0459] = 0x9A;     in_tab[0x9A] = 0x0459;
+      out_tab[0x203A] = 0x9B;     in_tab[0x9B] = 0x203A;
+      out_tab[0x045A] = 0x9C;     in_tab[0x9C] = 0x045A;
+      out_tab[0x045C] = 0x9D;     in_tab[0x9D] = 0x045C;
+      out_tab[0x045B] = 0x9E;     in_tab[0x9E] = 0x045B;
+      out_tab[0x045F] = 0x9F;     in_tab[0x9F] = 0x045F;
+      out_tab[0x00A0] = 0xA0;     in_tab[0xA0] = 0x00A0;
+      out_tab[0x040E] = 0xA1;     in_tab[0xA1] = 0x040E;
+      out_tab[0x045E] = 0xA2;     in_tab[0xA2] = 0x045E;
+      out_tab[0x0408] = 0xA3;     in_tab[0xA3] = 0x0408;
+      out_tab[0x00A4] = 0xA4;     in_tab[0xA4] = 0x00A4;
+      out_tab[0x0490] = 0xA5;     in_tab[0xA5] = 0x0490;
+      out_tab[0x00A6] = 0xA6;     in_tab[0xA6] = 0x00A6;
+      out_tab[0x00A7] = 0xA7;     in_tab[0xA7] = 0x00A7;
+      out_tab[0x0401] = 0xA8;     in_tab[0xA8] = 0x0401;
+      out_tab[0x00A9] = 0xA9;     in_tab[0xA9] = 0x00A9;
+      out_tab[0x0404] = 0xAA;     in_tab[0xAA] = 0x0404;
+      out_tab[0x00AB] = 0xAB;     in_tab[0xAB] = 0x00AB;
+      out_tab[0x00AC] = 0xAC;     in_tab[0xAC] = 0x00AC;
+      out_tab[0x00AD] = 0xAD;     in_tab[0xAD] = 0x00AD;
+      out_tab[0x00AE] = 0xAE;     in_tab[0xAE] = 0x00AE;
+      out_tab[0x0407] = 0xAF;     in_tab[0xAF] = 0x0407;
+      out_tab[0x00B0] = 0xB0;     in_tab[0xB0] = 0x00B0;
+      out_tab[0x00B1] = 0xB1;     in_tab[0xB1] = 0x00B1;
+      out_tab[0x0406] = 0xB2;     in_tab[0xB2] = 0x0406;
+      out_tab[0x0456] = 0xB3;     in_tab[0xB3] = 0x0456;
+      out_tab[0x0491] = 0xB4;     in_tab[0xB4] = 0x0491;
+      out_tab[0x00B5] = 0xB5;     in_tab[0xB5] = 0x00B5;
+      out_tab[0x00B6] = 0xB6;     in_tab[0xB6] = 0x00B6;
+      out_tab[0x00B7] = 0xB7;     in_tab[0xB7] = 0x00B7;
+      out_tab[0x0451] = 0xB8;     in_tab[0xB8] = 0x0451;
+      out_tab[0x2116] = 0xB9;     in_tab[0xB9] = 0x2116;
+      out_tab[0x0454] = 0xBA;     in_tab[0xBA] = 0x0454;
+      out_tab[0x00BB] = 0xBB;     in_tab[0xBB] = 0x00BB;
+      out_tab[0x0458] = 0xBC;     in_tab[0xBC] = 0x0458;
+      out_tab[0x0405] = 0xBD;     in_tab[0xBD] = 0x0405;
+      out_tab[0x0455] = 0xBE;     in_tab[0xBE] = 0x0455;
+      out_tab[0x0457] = 0xBF;     in_tab[0xBF] = 0x0457;
+=======
       out_tab[0x0402] = static_cast<unsigned char>(0x80);
       out_tab[0x0403] = static_cast<unsigned char>(0x81);
       out_tab[0x201A] = static_cast<unsigned char>(0x82);
@@ -150,6 +216,7 @@ class codecvt_cp1251 : public std::codecvt<wchar_t, char, mbstate_t>
       in_tab[static_cast<unsigned char>(0xBD)] = 0x0405;
       in_tab[static_cast<unsigned char>(0xBE)] = 0x0455;
       in_tab[static_cast<unsigned char>(0xBF)] = 0x0457;
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
     }
 
     ~codecvt_cp1251()
@@ -177,6 +244,20 @@ protected:
         to_next = to;
         return partial;
       }
+<<<<<<< HEAD
+      const unsigned char ufrom = static_cast<unsigned char>(*from);
+      // ASCII
+      if(ufrom <= 0x7F)
+        *to = static_cast<wchar_t>(*from);
+      else if(0xC0 <= ufrom)
+        *to = static_cast<wchar_t>(ufrom + 0x350);
+      else
+      {
+        std::map<unsigned char, wchar_t>::const_iterator s;
+        s = in_tab.lower_bound(ufrom);
+        if(s->first != ufrom || s == in_tab.end())
+        {
+=======
       // ASCII
       if(static_cast<unsigned char>(*from) <= 0x7F)
       {
@@ -195,6 +276,7 @@ protected:
         if(s->first != *from || s == in_tab.end())
         {
           // ok from and to is not endings
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
           from_next = ++from;
           to_next = ++to;
           return error;
@@ -228,6 +310,14 @@ protected:
         return partial;
       }
       if(0 <= *from && *from <= 0x7F)
+<<<<<<< HEAD
+        *to = static_cast<unsigned char>(*from);
+      else if(0x410 <= *from && *from <=0x44F)
+        *to = static_cast<unsigned char>(*from - 0x350);
+      else
+      {
+        std::map<wchar_t, unsigned char>::const_iterator s;
+=======
       {
         *to = static_cast<unsigned char>(*from);
       }
@@ -239,6 +329,7 @@ protected:
       else
       {
         std::map<wchar_t, char>::const_iterator s;
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
         s = out_tab.lower_bound(*from);
         if( s->first != *from || s == out_tab.end())
         {
@@ -278,8 +369,13 @@ protected:
   }
      */
 private:
+<<<<<<< HEAD
+  std::map<unsigned char, wchar_t> in_tab;
+  std::map<wchar_t, unsigned char> out_tab;
+=======
   std::map<char, wchar_t> in_tab;
   std::map<wchar_t, char> out_tab;
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
 
 };
 
--- codecvt/codecvt_cp866.hpp
+++ codecvt/codecvt_cp866.hpp
@@ -21,6 +21,72 @@ class codecvt_cp866 : public std::codecvt<wchar_t, char, mbstate_t>
         noconv(std::codecvt_base::noconv)
     {
       //  UNICODE -> CP866 table (extended part)
+<<<<<<< HEAD
+      out_tab[0x2591] = 0xb0;   in_tab[0xb0] = 0x2591;
+      out_tab[0x2592] = 0xb1;   in_tab[0xb1] = 0x2592;
+      out_tab[0x2593] = 0xb2;   in_tab[0xb2] = 0x2593;
+      out_tab[0x2502] = 0xb3;   in_tab[0xb3] = 0x2502;
+      out_tab[0x2524] = 0xb4;   in_tab[0xb4] = 0x2524;
+      out_tab[0x2561] = 0xb5;   in_tab[0xb5] = 0x2561;
+      out_tab[0x2562] = 0xb6;   in_tab[0xb6] = 0x2562;
+      out_tab[0x2556] = 0xb7;   in_tab[0xb7] = 0x2556;
+      out_tab[0x2555] = 0xb8;   in_tab[0xb8] = 0x2555;
+      out_tab[0x2563] = 0xb9;   in_tab[0xb9] = 0x2563;
+      out_tab[0x2551] = 0xba;   in_tab[0xba] = 0x2551;
+      out_tab[0x2557] = 0xbb;   in_tab[0xbb] = 0x2557;
+      out_tab[0x255d] = 0xbc;   in_tab[0xbc] = 0x255d;
+      out_tab[0x255c] = 0xbd;   in_tab[0xbd] = 0x255c;
+      out_tab[0x255b] = 0xbe;   in_tab[0xbe] = 0x255b;
+      out_tab[0x2510] = 0xbf;   in_tab[0xbf] = 0x2510;
+      out_tab[0x2514] = 0xc0;   in_tab[0xc0] = 0x2514;
+      out_tab[0x2534] = 0xc1;   in_tab[0xc1] = 0x2534;
+      out_tab[0x252c] = 0xc2;   in_tab[0xc2] = 0x252c;
+      out_tab[0x251c] = 0xc3;   in_tab[0xc3] = 0x251c;
+      out_tab[0x2500] = 0xc4;   in_tab[0xc4] = 0x2500;
+      out_tab[0x253c] = 0xc5;   in_tab[0xc5] = 0x253c;
+      out_tab[0x255e] = 0xc6;   in_tab[0xc6] = 0x255e;
+      out_tab[0x255f] = 0xc7;   in_tab[0xc7] = 0x255f;
+      out_tab[0x255a] = 0xc8;   in_tab[0xc8] = 0x255a;
+      out_tab[0x2554] = 0xc9;   in_tab[0xc9] = 0x2554;
+      out_tab[0x2569] = 0xca;   in_tab[0xca] = 0x2569;
+      out_tab[0x2566] = 0xcb;   in_tab[0xcb] = 0x2566;
+      out_tab[0x2560] = 0xcc;   in_tab[0xcc] = 0x2560;
+      out_tab[0x2550] = 0xcd;   in_tab[0xcd] = 0x2550;
+      out_tab[0x256c] = 0xce;   in_tab[0xce] = 0x256c;
+      out_tab[0x2567] = 0xcf;   in_tab[0xcf] = 0x2567;
+      out_tab[0x2568] = 0xd0;   in_tab[0xd0] = 0x2568;
+      out_tab[0x2564] = 0xd1;   in_tab[0xd1] = 0x2564;
+      out_tab[0x2565] = 0xd2;   in_tab[0xd2] = 0x2565;
+      out_tab[0x2559] = 0xd3;   in_tab[0xd3] = 0x2559;
+      out_tab[0x2558] = 0xd4;   in_tab[0xd4] = 0x2558;
+      out_tab[0x2552] = 0xd5;   in_tab[0xd5] = 0x2552;
+      out_tab[0x2553] = 0xd6;   in_tab[0xd6] = 0x2553;
+      out_tab[0x256b] = 0xd7;   in_tab[0xd7] = 0x256b;
+      out_tab[0x256a] = 0xd8;   in_tab[0xd8] = 0x256a;
+      out_tab[0x2518] = 0xd9;   in_tab[0xd9] = 0x2518;
+      out_tab[0x250c] = 0xda;   in_tab[0xda] = 0x250c;
+      out_tab[0x2588] = 0xdb;   in_tab[0xdb] = 0x2588;
+      out_tab[0x2584] = 0xdc;   in_tab[0xdc] = 0x2584;
+      out_tab[0x258c] = 0xdd;   in_tab[0xdd] = 0x258c;
+      out_tab[0x2590] = 0xde;   in_tab[0xde] = 0x2590;
+      out_tab[0x2580] = 0xdf;   in_tab[0xdf] = 0x2580;
+      out_tab[0x0401] = 0xf0;   in_tab[0xf0] = 0x0401;
+      out_tab[0x0451] = 0xf1;   in_tab[0xf1] = 0x0451;
+      out_tab[0x0404] = 0xf2;   in_tab[0xf2] = 0x0404;
+      out_tab[0x0454] = 0xf3;   in_tab[0xf3] = 0x0454;
+      out_tab[0x0407] = 0xf4;   in_tab[0xf4] = 0x0407;
+      out_tab[0x0457] = 0xf5;   in_tab[0xf5] = 0x0457;
+      out_tab[0x040e] = 0xf6;   in_tab[0xf6] = 0x040e;
+      out_tab[0x045e] = 0xf7;   in_tab[0xf7] = 0x045e;
+      out_tab[0x00b0] = 0xf8;   in_tab[0xf8] = 0x00b0;
+      out_tab[0x2219] = 0xf9;   in_tab[0xf9] = 0x2219;
+      out_tab[0x00b7] = 0xfa;   in_tab[0xfa] = 0x00b7;
+      out_tab[0x221a] = 0xfb;   in_tab[0xfb] = 0x221a;
+      out_tab[0x2116] = 0xfc;   in_tab[0xfc] = 0x2116;
+      out_tab[0x00a4] = 0xfd;   in_tab[0xfd] = 0x00a4;
+      out_tab[0x25a0] = 0xfe;   in_tab[0xfe] = 0x25a0;
+      out_tab[0x00a0] = 0xff;   in_tab[0xff] = 0x00a0;
+=======
       out_tab[0x2591] = static_cast<unsigned char>(0xb0);
       out_tab[0x2592] = static_cast<unsigned char>(0xb1);
       out_tab[0x2593] = static_cast<unsigned char>(0xb2);
@@ -150,6 +216,7 @@ class codecvt_cp866 : public std::codecvt<wchar_t, char, mbstate_t>
       in_tab[static_cast<unsigned char>(0xfd)] = 0x00a4;
       in_tab[static_cast<unsigned char>(0xfe)] = 0x25a0;
       in_tab[static_cast<unsigned char>(0xff)] = 0x00a0;
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
     }
 
     ~codecvt_cp866()
@@ -174,6 +241,21 @@ protected:
         to_next = to;
         return partial;
       }
+<<<<<<< HEAD
+      const unsigned char ufrom = static_cast<unsigned char>(*from);
+      // ASCII
+      if(ufrom <= 0x7F)
+        *to = static_cast<wchar_t>(*from);
+      else if(0x80 <= ufrom && ufrom <= 0xAF)
+        *to = static_cast<wchar_t>(ufrom + 0x390);
+      else if(0xE0 <= ufrom && ufrom <= 0xEF)
+        *to = static_cast<wchar_t>(ufrom + 0x360);
+      else
+      {
+        std::map<unsigned char, wchar_t>::const_iterator s;
+        s = in_tab.lower_bound(ufrom);
+        if(s->first != ufrom || s == in_tab.end())
+=======
       // ASCII
       if(static_cast<unsigned char>(*from) <= 0x7F)
       {
@@ -196,6 +278,7 @@ protected:
         std::map<char, wchar_t>::const_iterator s;
         s = in_tab.lower_bound(*from);
         if(s->first != *from || s == in_tab.end())
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
         {
           from_next = ++from;
           to_next = ++to;
@@ -228,6 +311,17 @@ protected:
         to_next = to;
         return partial;
       }
+<<<<<<< HEAD
+      if(0 <= *from && *from <= 0x7F)
+        *to = static_cast<unsigned char>(*from);
+      else if(0x410 <= *from && *from <= 0x43F)
+        *to = static_cast<unsigned char>(*from - 0x390);
+      else if(0x440 <= *from && *from <=0x44F)
+        *to = static_cast<unsigned char>(*from - 0x360);
+      else
+      {
+        std::map<wchar_t, unsigned char>::const_iterator s;
+=======
       if(static_cast<unsigned char>(*from) <= 0x7F)
       {
         *to = static_cast<unsigned char>(*from);
@@ -245,6 +339,7 @@ protected:
       else
       {
         std::map<wchar_t, char>::const_iterator s;
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
         s = out_tab.lower_bound(*from);
         if(s->first != *from || s == out_tab.end())
         {
@@ -283,9 +378,14 @@ protected:
   }
      */
 private:
+<<<<<<< HEAD
+  std::map<unsigned char, wchar_t> in_tab;
+  std::map<wchar_t, unsigned char> out_tab;
+=======
   std::map<char, wchar_t> in_tab;
   std::map<wchar_t, char> out_tab;
 
+>>>>>>> 76915525ccfd763c294d5b178d4662633dc6de14
 };
 
 #endif // __CODECVT_CP866__
